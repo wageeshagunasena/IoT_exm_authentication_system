afeb935ec75a0bb0216cce858a2f3a6b599092cd
Wageesha Sarith Gunasena <wageesha.2012@gmail.com>
2017-08-02 02:15:24 +0530

    Add files via upload

---
+++ Server/src/Server/EmailSend.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Server;
+
+import java.io.File;
+import java.util.Properties;
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+/**
+ *
+ * @author WAGEESHA-PC
+ */
+public class EmailSend {
+
+    public void send(String subject,String email) {
+
+        try {
+//            Set credentials
+            String host = "smtp.gmail.com";
+            String user = "wagguna.123@gmail.com";
+            String pass = "0702017667";
+            String to = email;
+            String from = "wagguna.123@gmail.com";
+
+            boolean sessionDebug = false;
+
+            Properties props = System.getProperties();
+//          Set smtp properties
+            props.put("mail.smtp.starttls.enable", "true");
+            props.put("mail.smtp.host", host);
+            props.put("mail.smtp.port", 587);
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.starttls.required", "true");
+
+            java.security.Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
+            Session mailSession = Session.getDefaultInstance(props, null);
+            mailSession.setDebug(sessionDebug);
+            Message msg = new MimeMessage(mailSession);
+            msg.setFrom(new InternetAddress(from));
+            InternetAddress[] address = {new InternetAddress(to)};
+            msg.setRecipients(Message.RecipientType.TO, address);
+            msg.setSubject("Attendance list of "+subject);
+            msg.setText("");
+
+            Multipart multipart = new MimeMultipart();
+
+            MimeBodyPart textBodyPart = new MimeBodyPart();
+            textBodyPart.setText("");
+
+//            Attach file to email
+            MimeBodyPart attachmentBodyPart = new MimeBodyPart();
+            File attachementPath;
+            DataSource source = new FileDataSource("ATTD"); // ex : "C:\\test.pdf"
+            attachmentBodyPart.setDataHandler(new DataHandler(source));
+            attachmentBodyPart.setFileName("ATTD"); // ex : "test.pdf"
+
+            multipart.addBodyPart(textBodyPart);  // add the text part
+            multipart.addBodyPart(attachmentBodyPart); // add the attachement part
+
+            msg.setContent(multipart);
+
+//            Send Email
+            Transport transport = mailSession.getTransport("smtp");
+            transport.connect(host, user, pass);
+            transport.sendMessage(msg, msg.getAllRecipients());
+            transport.close();
+//            Transport.send(msg);
+            System.out.println("Message send");
+        } catch (MessagingException messagingException) {
+            messagingException.printStackTrace();
+        }
+
+    }
+
+}
---
+++ Server/src/Server/TCPServer.java
@@ -0,0 +1,139 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Server;
+
+import java.io.*;
+import java.net.*;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+public class TCPServer {
+    
+//    Define server socket
+    static ServerSocket welcomeSocket;
+    
+//    Define student details
+    static String studID;
+    static String studName;
+    static String subCode;
+    static ArrayList<String> attd = new ArrayList<>();
+
+    public static void main(String argv[]) throws Exception {
+//        Set server socket to port 5000
+        welcomeSocket = new ServerSocket(5000);
+        
+//        new EmailSend().send("EC4421");
+//      Listen to connections
+        while (true) {
+
+            String way = recieveData();
+            System.out.println(way);
+
+            if (way.equals("f")) {
+                subCode = recieveData();
+                String fpID = recieveData();
+                System.out.println(subCode);
+                System.out.println(fpID);
+
+                getStudentfp(subCode, fpID);
+
+                System.out.println(studID);
+                System.out.println(studName);
+
+                attd.add(studID + " FPS");
+
+                sendData("y," + studID + "," + studName + ",\n");
+            } else if (way.equals("b")) {
+
+                subCode = recieveData();
+                String fpID = recieveData();
+                String magID = recieveData();
+                System.out.println(subCode);
+                System.out.println(fpID);
+                System.out.println(magID);
+
+                getStudentboth(subCode, fpID, magID);
+
+                System.out.println(studID);
+                System.out.println(studName);
+
+                attd.add(studID + " FPS & Card");
+
+                sendData("y," + studID + "," + studName + ",\n");
+
+            } else if(way.equals("k")){
+                System.out.println("write");
+                writeFile();
+            }
+
+        }
+
+    }
+
+//    Receive data through port 5000 when socket connected
+    static String recieveData() throws Exception {
+        String data;
+        Socket consoc = welcomeSocket.accept();
+        System.out.println("got");
+        BufferedReader inFromDevice = new BufferedReader(new InputStreamReader(consoc.getInputStream()));
+        data = inFromDevice.readLine();
+        consoc.close();
+        return data;
+    }
+    
+//  Send data through port 5000 when socket connected
+    static void sendData(String student) throws Exception {
+        Socket consoc = welcomeSocket.accept();
+        BufferedWriter outToDevice = new BufferedWriter(new OutputStreamWriter(consoc.getOutputStream()));
+        outToDevice.write(student);
+        outToDevice.flush();
+        consoc.close();
+    }
+
+//    Get student from data base when finger-print only option choosed
+    public static void getStudentfp(String subID, String fpID) throws Exception {
+
+//        Get data to a resulset from data base, SQL query written
+        ResultSet rset = new jdb().getdata("SELECT s.Student_ID, s.Student_Name FROM student s, students_courses c "
+                + "WHERE s.Student_ID=c.Student_ID AND c.Subject_ID='" + subID + "' AND s.Finger_ID='" + fpID + "'");
+
+//        Set student details
+        while (rset.next()) {
+            studID = rset.getString(1);
+            studName = rset.getString(2);
+        }
+
+    }
+
+//    Get student details from databse when bothways option choosed
+    public static void getStudentboth(String subID, String fpID, String magID) throws Exception {
+
+//        Get data to a resulset from data base, SQL query written        
+        ResultSet rset = new jdb().getdata("SELECT s.Student_ID, s.Student_Name FROM student s, students_courses c "
+                + "WHERE s.Student_ID=c.Student_ID AND c.Subject_ID='" + subID + "' AND s.Finger_ID='" + fpID + "' "
+                + "AND s.Card_ID='" + magID + "'");
+
+        //        Set student details
+        while (rset.next()) {
+            studID = rset.getString(1);
+            studName = rset.getString(2);
+        }
+
+    }
+
+//    write the final details into a file
+    public static void writeFile() throws Exception{
+        File file = new File("ATTD");
+        PrintWriter writer = new PrintWriter(file);
+        for (String string : attd) {
+            writer.println(string);
+        }
+        writer.close();
+//        send file to defined email
+        new EmailSend().send(subCode,"wageesha.2012@gmail.com");
+//        sendData("OK");
+    }
+}
