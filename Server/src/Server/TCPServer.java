630f84cd2902a3bee43d96776eeb85fb6f47e4da
Wageesha Sarith Gunasena <wageesha.2012@gmail.com>
2017-08-02 02:29:50 +0530

    Update TCPServer.java

--- Server/src/Server/TCPServer.java
+++ Server/src/Server/TCPServer.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * The server class
+ * This server and the device should run on the same subnet
+ * 
  */
 package Server;
 
